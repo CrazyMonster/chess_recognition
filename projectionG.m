d55230ccc235577bc27d34789f0cb9dcedc94f3f
Giorgia Aurora Adorni <adornigiorgia@outlook.it>
2017-12-24 18:36:38 +0100

    Aggiunto codice che proietta la scacchiera in un quadrato

--- projectionG.m
+++ projectionG.m
@@ -7,13 +7,17 @@ if ~exist('datasets/1/tmp/projected', 'dir')
     mkdir('datasets/1/tmp/projected');
 end
 
-
 %%
-parfor i = 1:size(ds, 1)
-   im = imread(['datasets/' num2str(id) '/images/' images{i} '.jpg']);
+for i = 13
+    im = imread(['datasets/' num2str(id) '/tmp/gray/' images{i} '.jpg']);
+    
+    movingPoints = [483 1281; 1899 1365; 435 2811; 1829 2709];
+    fixedPoints = [0 0; 500 0; 0 500; 500 500];
+    
+    % matrice di trasformazione
+    tform = fitgeotrans(movingPoints, fixedPoints, 'projective');
+    
+    projected = imwarp(im, tform, 'OutputView', imref2d([500, 500]));
    
-   
-   
-   imwrite(im, ['datasets/' num2str(id) '/tmp/projected/' images{i} '.jpg']);
+    imwrite(projected, ['datasets/' num2str(id) '/tmp/projected/' images{i} '.jpg']);
 end
-
