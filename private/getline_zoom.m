5a5baf67f2a1d3569f1aef7f353376c881255963
Elia Cereda <eliacereda@gmail.com>
2017-12-26 15:57:31 +0100

    Inserita la licenza e l’URL di provenienza per le funzioni imread_rotate e getline_zoom

--- private/getline_zoom.m
+++ private/getline_zoom.m
@@ -1,6 +1,34 @@
-%Tristan Ursell
-%November 2013
-%Active zoom for polyline selection.
+% Copyright (c) 2013, Tristan Ursell
+% Copyright (c) 2012, John D'Errico
+% All rights reserved.
+% 
+% Redistribution and use in source and binary forms, with or without
+% modification, are permitted provided that the following conditions are
+% met:
+% 
+%     * Redistributions of source code must retain the above copyright
+%       notice, this list of conditions and the following disclaimer.
+%     * Redistributions in binary form must reproduce the above copyright
+%       notice, this list of conditions and the following disclaimer in
+%       the documentation and/or other materials provided with the distribution
+% 
+% THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+% AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+% IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+% ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+% LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+% CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+% SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+% INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+% CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+% ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+% POSSIBILITY OF SUCH DAMAGE.
+
+% Tristan Ursell
+% November 2013
+% Active zoom for polyline selection.
+%
+% Source: https://it.mathworks.com/matlabcentral/fileexchange/38959-active-figure-zoom-for-selecting-points
 %
 % [X,Y]=getline_zoom(Im1);
 % [X,Y]=getline_zoom(Im1,'plot');
@@ -160,7 +188,7 @@ while true
     end
     
     %select next point
-    [X_temp,Y_temp,flag0]=ginput(1);
+    [X_temp, Y_temp, ~]=ginput(1);
     
     %set new zoom center
     X0=X_temp+xl-1;
--- private/imread_rotate.m
+++ private/imread_rotate.m
@@ -1,26 +1,31 @@
-function IM = imread_rotate(filename)
-    IM = imread(filename);
+% Walter Roberson
+% December 2015
+% Source: https://it.mathworks.com/matlabcentral/answers/260607-how-to-load-a-jpg-properly#answer_203481
+
+function im = imread_rotate(filename)
+    im = imread(filename);
     info = imfinfo(filename);
 
     if isfield(info,'Orientation')
        orient = info(1).Orientation;
+       
        switch orient
          case 1
             %normal, leave the data alone
          case 2
-            IM = IM(:,end:-1:1,:);         %right to left
+            im = im(:,end:-1:1,:);         %right to left
          case 3
-            IM = IM(end:-1:1,end:-1:1,:);  %180 degree rotation
+            im = im(end:-1:1,end:-1:1,:);  %180 degree rotation
          case 4
-            IM = IM(end:-1:1,:,:);         %bottom to top
+            im = im(end:-1:1,:,:);         %bottom to top
          case 5
-            IM = permute(IM, [2 1 3]);     %counterclockwise and upside down
+            im = permute(im, [2 1 3]);     %counterclockwise and upside down
          case 6
-            IM = rot90(IM,3);              %undo 90 degree by rotating 270
+            im = rot90(im,3);              %undo 90 degree by rotating 270
          case 7
-            IM = rot90(IM(end:-1:1,:,:));  %undo counterclockwise and left/right
+            im = rot90(im(end:-1:1,:,:));  %undo counterclockwise and left/right
          case 8
-            IM = rot90(IM);                %undo 270 rotation by rotating 90
+            im = rot90(im);                %undo 270 rotation by rotating 90
          otherwise
             warning('unknown orientation %g ignored\n', orient);
        end
