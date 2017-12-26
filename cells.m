8f66967ab7bfa742470d29b21a1c6a3322120992
Giorgia Aurora Adorni <adornigiorgia@outlook.it>
2017-12-26 15:48:53 +0100

    Aggiunto script cells.m
    - estrae ogni cella dalla scacchiera

---
+++ cells.m
@@ -0,0 +1,28 @@
+id = 2;
+ds = load_dataset(id);
+
+images = ds.Labels.Image;
+
+parfor i = 1:size(images, 1)
+    
+    if ~exist(['datasets/' num2str(id) '/tmp/cells/' images{i}], 'dir')
+        mkdir(['datasets/' num2str(id) '/tmp/cells/' images{i}]);
+    end
+    
+    for j = 1:8
+        for l = 1:8
+            
+            x = 64 * (j - 1);
+            y = 64 * (l - 1);
+            
+            im = imread(['datasets/' num2str(id) '/tmp/cropped/projectedgray/' images{i} '.jpg']);
+            im_cropped = imcrop(im, [x, y, 64, 64]);
+            
+            imwrite(im_cropped, ['datasets/' num2str(id) '/tmp/cells/' images{i} '/' num2str(j) 'x' num2str(l) '.jpg']);
+  
+        end 
+    end
+    
+    
+    
+end
