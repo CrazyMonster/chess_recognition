da35191be8b8350e67ded31ff17837624dc26288
Elia Cereda <eliacereda@gmail.com>
2017-12-26 13:48:15 +0100

    Aggiornata la funzione load_dataset e lo script plot_frame.
     * load_dataset ora carica anche le cornici delle scacchiere
    * plot_frame ora usa il nuovo formato ritornato da load_dataset.

--- load_dataset.m
+++ load_dataset.m
@@ -1,8 +1,34 @@
-function out = load_dataset(id)
-    filename = ['datasets/' num2str(id) '/labels.csv'];
+function ds = load_dataset(id)
+    ds.Path = ["datasets", num2str(id)];
     
-    opts = detectImportOptions(filename);
-    opts = setvartype(opts, {'Image'}, 'char');
+    ds.Labels = load_labels(ds);
+    ds.Labels.FramePoints = load_frames(ds);
+end
+
+function out = path_for_asset(dataset, filename)
+    out = join([dataset.Path, filename], "/");
     
-    out = readtable(filename, opts);
+    % Converti il path in character array, come richiesto da molte funzioni
+    % di Matlab.
+    out = char(out);
+end
+
+function out = load_labels(dataset)
+    path = path_for_asset(dataset, "labels.csv");
+
+    opts = detectImportOptions(path);
+    
+    % Carica la colonna Image come character array.
+    opts = setvartype(opts, {'Image'}, 'string');
+    
+    out = readtable(path, opts);
+end
+
+function out = load_frames(dataset)
+   path = path_for_asset(dataset, "frame_points.mat");
+   
+   if exist(path, 'file')
+       fp = load(path);
+       out = fp.points;
+   end
 end
--- plot_frame.m
+++ plot_frame.m
@@ -1,31 +1,36 @@
 id = 1;
 ds = load_dataset(id);
 
-images = ds.Image;
-load(['datasets/' num2str(id) '/points.mat']);
+labels = ds.Labels;
 
-for i = 1:size(ds, 1)
-    im = imread_rotate(['datasets/' num2str(id) '/images/' images{i} '.jpg']);
+if ~exist(['datasets/' num2str(id) '/tmp/frame'], 'dir')
+    mkdir(['datasets/' num2str(id) '/tmp/frame']);
+end
+
+parfor i = 1:size(labels, 1)
+    l = labels(i, :);
+    
+    im = imread_rotate(['datasets/' num2str(id) '/images/' char(l.Image) '.jpg']);
+    fp = reshape(l.FramePoints, 4, 2);
     
     f1 = figure(1);
     
     imshow(im);
     hold on;
     
-    plot(points(i, 1:2, 1), points(i, 1:2, 2),'LineWidth', 2, 'Color', 'green');
-    plot(points(i, 2:2:4, 1), points(i, 2:2:4, 2),'LineWidth', 2, 'Color', 'green');
-    plot(points(i, 3:4, 1), points(i, 3:4, 2),'LineWidth', 2, 'Color', 'green');
-    plot(points(i, 1:2:3, 1), points(i, 1:2:3, 2),'LineWidth', 2, 'Color', 'green');
+    plot(fp(1:2, 1), fp(1:2, 2),'LineWidth', 2, 'Color', 'green');
+    plot(fp(2:2:4, 1), fp(2:2:4, 2),'LineWidth', 2, 'Color', 'green');
+    plot(fp(3:4, 1), fp(3:4, 2),'LineWidth', 2, 'Color', 'green');
+    plot(fp(1:2:3, 1), fp(1:2:3, 2),'LineWidth', 2, 'Color', 'green');
     
     for j=1:4
-        x = points(i, j, 1);
-        y = points(i, j, 2);
+        x = fp(j, 1);
+        y = fp(j, 2);
         
         plot(x, y, 'x', 'color', 'red');
     end
     
     hold off;
     
-    saveas(f1, ['datasets/' num2str(id) '/tmp/bbox/' images{i} '.jpg']);
-    
+    saveas(f1, ['datasets/' num2str(id) '/tmp/frame/' char(l.Image) '.jpg']);
 end
