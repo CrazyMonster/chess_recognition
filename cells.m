58badf7fe5d9d213da43235002286f355a50dd7f
Giorgia Aurora Adorni <adornigiorgia@outlook.it>
2018-01-12 17:40:33 +0100

    Corretta dimensione di output delle celle in 64x64

--- cells.m
+++ cells.m
@@ -12,15 +12,17 @@ parfor i = 1:size(images, 1)
     for j = 1:8
         for l = 1:8
             
-            x = 64 * (j - 1);
-            y = 64 * (l - 1);
+            x = 64 * (j - 1) + 1;
+            y = 64 * (l - 1) + 1;
             
             im = imread(['datasets/' num2str(id) '/tmp_G/cropped/projectedgray/' images{i} '.jpg']);
-            im_cropped = imcrop(im, [x, y, 64, 64]);
+            
+            im_cropped = im(x : x + 63, y : y + 63);
+              
+            %im_cropped = imcrop(im, [x, y, 63.5, 63.5]);
             
             imwrite(im_cropped, ['datasets/' num2str(id) '/tmp_G/cells/' images{i} '/' num2str(j) 'x' num2str(l) '.jpg']);
   
         end 
-    end
-    
+    end 
 end
