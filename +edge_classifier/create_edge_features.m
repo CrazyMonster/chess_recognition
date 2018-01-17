706ba4c844b07fc4eb0890040d4307abee2efd75
Elia Cereda <eliacereda@gmail.com>
2018-01-17 12:45:08 +0100

    Aggiornata create_edge_features per gestire immagini in cui sia presente un numero arbitrario di scacchiere

--- +edge_classifier/create_edge_features.m
+++ +edge_classifier/create_edge_features.m
@@ -2,28 +2,33 @@ function create_edge_features(id)
     ds = load_dataset(id);
     images = ds.Labels.Image;
     
-    input_dir = 'tmp/regionprops';
+    input_dir = 'tmp/regionprops_labeled';
     output_dir = 'tmp/edge_features';
 
     features = table();
     
     for i = 1:size(images, 1)
-        files = dir(['datasets/' num2str(id) '/' input_dir '/' images{i} '/+*.png']);
-        
-        assert(size(files, 1) == 1, ['Annotazione errata per immagine ' images{i} ]);
-        
-        matches = regexp(files(1).name, '(\d)+', 'match');
-        idx = str2double(matches{1});
-        
         m = matfile(['datasets/' num2str(id) '/' input_dir '/' images{i} '/' 'props.mat']);
         props = m.props;
         
+        props.Id(:) = 1:size(props, 1);
         props.Image(:) = images(i);
         
         props.IsROI(:) = false;
-        props.IsROI(idx) = true;
         
-        features = [features; props];
+        files = dir(['datasets/' num2str(id) '/' input_dir '/' images{i} '/+*.png']);
+        
+        for j = 1:size(files, 1)
+            matches = regexp(files(j).name, '(\d)+', 'match');
+            idx = str2double(matches{1});
+        
+            props.IsROI(idx) = true;
+        end
+            
+        % Il numero di righe aggiunte varia a seconda dell'immagine, non
+        % è possibile preallocare l'array. Il commento sotto disattiva il 
+        % warning di MATLAB.
+        features = [features; props]; %#ok<AGROW>
     end
     
     if ~exist(['datasets/' num2str(id) '/' output_dir], 'dir')
