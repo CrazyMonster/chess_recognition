7a778770acabccc05ed199f5eeaece1d23714cf0
Elia Cereda <eliacereda@gmail.com>
2018-02-10 18:34:05 +0100

    Aggiunti la generazione delle OrientationFlags a orientation_classifier/training/extract_features

--- +orientation_classifier/+training/extract_features.m
+++ +orientation_classifier/+training/extract_features.m
@@ -6,14 +6,16 @@ function out = extract_features(ds)
     n = height(labels);
     features = cell(n, 4);
     
-    for i = 1:n
+    parfor i = 1:n
         l = labels(i, :);
         
         % Segnala a MATLAB che questa è intenzionalmente una variabile broadcast. 
         [~] = path_for_asset;
         
         path = path_for_asset(["tmp", "eq", l.Image], "jpg");
+        
         image = imread(path);
+        board = board_info(l.BoardConfiguration);
         
         for o = 1:4
             f = orientation_classifier.extract_orientation_features(image);
@@ -21,15 +23,43 @@ function out = extract_features(ds)
             f.Dataset(:) = id;
             f.Image(:) = l.Image;
             f.Orientation(:) = o;
-
-            % Riordina le colonne in modo che le tre appena aggiunte compaiano per prime.
-            f = [f(:, end-2:end), f(:, 1:end-3)];
-
+            f.Cell = [board.X; board.Y]';
+            
+            % Riordina le colonne in modo che le quattro appena aggiunte compaiano per prime.
+            f = [f(:, end-3:end), f(:, 1:end-4)];
+            
+            f.OrientationFlags = orientation_flags(board, o);
+            
             features{i, o} = f;
             
             image = rot90(image, -1);
+            board = rot90(board, -1);
         end
     end
     
     out = vertcat(features{:});
+end
+
+function out = board_info(fen)
+    board = parse_fen(fen);
+    
+    X = repmat((1:8)', 1, 8);
+    Y = repmat((1:8), 8, 1);
+    
+    is_empty = (board == '*');
+    is_white = (mod(X + Y, 2) == 0);
+
+    out = struct('Piece', num2cell(board), ...
+                 'X', num2cell(X), 'Y', num2cell(Y), ...
+                 'IsEmpty', num2cell(is_empty), 'IsWhite', num2cell(is_white));
+end
+
+function out = orientation_flags(board, orientation)
+    flags = dec2bin(orientation - 1, 2);
+    is_empty = [board.IsEmpty];
+    is_white = [board.IsWhite];
+
+    out = repmat(flags, 64, 1);
+    out(is_empty, 1) = '*';
+    out(is_empty & is_white, 2) = '*';
 end
