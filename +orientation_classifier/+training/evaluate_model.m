5ef8d6312b9873778b329d051e4a7477006095ef
Elia Cereda <eliacereda@gmail.com>
2018-02-10 20:13:05 +0100

    Aggiunte le funzioni evaluate_model e count_predicted_flags a orientation_classifier

---
+++ +orientation_classifier/+training/evaluate_model.m
@@ -0,0 +1,38 @@
+function evaluate_model(model, dataset)
+    gt = dataset.OrientationFlags;
+    predictions = model.predictFcn(dataset);
+    
+    cm = confmat(string(gt), string(predictions));
+    
+    labels = strrep(cm.labels, '*', '_');
+    plabels = strcat('P', cellstr(labels));
+    tlabels = strcat('T', cellstr(labels));
+    
+    fprintf('<strong>Accuracy:</strong> %.2f%%\n', cm.accuracy * 100);
+    fprintf('\n');
+    fprintf('<strong>Confusion Matrix (relative)</strong>\n');
+    disp(array2table(cm.cm, 'VariableNames', plabels, 'RowNames', tlabels));
+    fprintf('\n');
+    fprintf('<strong>Confusion Matrix (absolute)</strong>\n');
+    disp(array2table(cm.cm_raw, 'VariableNames', plabels, 'RowNames', tlabels));
+    
+    images = unique(dataset(:, {'Dataset', 'Image', 'Orientation'}));
+    
+    for i = 1:height(images)
+        image = images(i, :);
+        
+        [~, idx, ~] = innerjoin(dataset, image);
+        p = predictions(idx, :);
+        
+        [orientation, v] = orientation_classifier.count_predited_flags(p);
+        
+        if image.Orientation == orientation
+            ok = 'OK';
+        else
+            ok = 'ERR';
+        end
+        
+        fprintf('Dataset %d Image %s: T%d => P%d (%.2f votes) %s\n', ...
+                image.Dataset, image.Image, image.Orientation, orientation, v, ok);
+    end
+end
