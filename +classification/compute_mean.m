4b453fbeb026a60aef67baf26f92b783ad8aad21
Elia Cereda <eliacereda@gmail.com>
2018-02-12 15:24:20 +0100

    Rimossa vari script e funzioni non più necessari

--- +classification/compute_mean.m
+++
@@ -1,8 +0,0 @@
-function out = compute_mean(image)
-
-    image = im2double(image);
-    [rows, cols, chs] = size(image);
-    out = mean(reshape(image, [rows * cols, chs]));
-   
-end
-
--- +classification/compute_stdev.m
+++
@@ -1,8 +0,0 @@
-function out = compute_stdev(image)
-
-    image = im2double(image);
-    [rows, cols, chs] = size(image);
-    out = std(reshape(image, [rows * cols, chs]));
-
-end
-
