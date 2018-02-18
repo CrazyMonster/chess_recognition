488f1646b69264ef57b09b45a08fb76c137d8ff6
Elia Cereda <eliacereda@gmail.com>
2018-02-18 13:03:23 +0100

    Aggiornato lo script utilities.copy_predicted per utilizzare la nuova struttura della cartella tmp/

--- +utilities/copy_predicted.m
+++ +utilities/copy_predicted.m
@@ -1,15 +1,16 @@
 id = 1;
-r = edge_classifier.training.load_roi(id);
+ds = load_dataset(id);
+r = edge_classifier.training.load_roi(ds);
 
-[~, ~] = mkdir(['datasets/' num2str(id) '/tmp/11.predicted/']);
+[~, ~] = mkdir(['datasets/' num2str(id) '/tmp/predicted/']);
 
 r = r(r.IsROI == 1, :);
 
 parfor j=1:height(r)
     region = r(j, :);
     
-    in = ['datasets/' num2str(id) '/frames/' char(region.Image) '/+' num2str(region.Region) '.png'];
-    out = ['datasets/' num2str(id) '/tmp/11.predicted/' char(region.Image) '.png'];
+    in = ['datasets/' num2str(id) '/tmp/edge_classifier/09.masked/' char(region.Image) '/' num2str(region.Region) '.jpg'];
+    out = ['datasets/' num2str(id) '/tmp/predicted/' char(region.Image) '.jpg'];
     
     copyfile(in, out);
 end
