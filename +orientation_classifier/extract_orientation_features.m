9a2d2be9623d55570211e2604fb3cffc875d5340
Giorgia Aurora Adorni <adornigiorgia@outlook.it>
2018-02-18 19:08:37 +0100

    Aggiunte feature all’orientation_classifier
    Aggiunta proiezione e modificati i parametri di HOG (aumentate la feature di HOG)

--- +orientation_classifier/extract_orientation_features.m
+++ +orientation_classifier/extract_orientation_features.m
@@ -1,10 +1,21 @@
 function out = extract_orientation_features(image)
     cols = im2col(double(image), [64 64], 'distinct');
-    hog = extractHOGFeatures(image, 'CellSize', [32 32], 'NumBins', 6, 'BlockSize', [2 2], 'BlockOverlap', [0 0]);
+    hog = extractHOGFeatures(image, 'CellSize', [32 32], 'NumBins', 9, 'BlockSize', [2 2], 'BlockOverlap', [0 0]);
+    
+    projectionX = blockproc(image, [64 64], @(block) compute_sum(block, 1)); 
+    projectionY = blockproc(image, [64 64], @(block) compute_sum(block, 2)); 
     
     out = table;
     
     out.Mean = mean(cols)';
     out.Stdev = std(cols)';
-    out.HOG = reshape(hog, 24, 64)';
+    out.HOG = reshape(hog, 36, 64)';
+    out.ProjectionX = reshape(projectionX, [], 64);
+    out.ProjectionY = reshape(projectionY, [], 64);
+    
+end
+
+function out = compute_sum(block, dim)
+    s = sum(block.data, dim);
+    out = reshape(s, 1, 1, []);
 end
