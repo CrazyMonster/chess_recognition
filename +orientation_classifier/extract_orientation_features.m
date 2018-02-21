03a478313c6644dcddf82b6e564a7d8324caaae9
Elia Cereda <eliacereda@gmail.com>
2018-02-21 16:45:41 +0100

    Modificata extract_orientation_features per calcolare le proiezioni in un’unica chiamata a blockproc

--- +orientation_classifier/extract_orientation_features.m
+++ +orientation_classifier/extract_orientation_features.m
@@ -1,21 +1,21 @@
 function out = extract_orientation_features(image)
     cols = im2col(double(image), [64 64], 'distinct');
     hog = extractHOGFeatures(image, 'CellSize', [32 32], 'NumBins', 9, 'BlockSize', [2 2], 'BlockOverlap', [0 0]);
-    
-    projectionX = blockproc(image, [64 64], @(block) compute_sum(block, 1)); 
-    projectionY = blockproc(image, [64 64], @(block) compute_sum(block, 2)); 
+    projections = blockproc(image, [64 64], @compute_sum); 
     
     out = table;
     
     out.Mean = mean(cols)';
     out.Stdev = std(cols)';
     out.HOG = reshape(hog, 36, 64)';
-    out.ProjectionX = reshape(projectionX, [], 64);
-    out.ProjectionY = reshape(projectionY, [], 64);
-    
+    out.ProjectionX = reshape(projections(:, :, 1:64), [], 64);
+    out.ProjectionY = reshape(projections(:, :, 65:128), [], 64);
 end
 
-function out = compute_sum(block, dim)
-    s = sum(block.data, dim);
-    out = reshape(s, 1, 1, []);
+function out = compute_sum(block)
+    sX = sum(block.data, 1);
+    sY = sum(block.data, 2);
+    
+    out(1, 1, 1:64) = reshape(sX, 1, 1, []);
+    out(1, 1, 65:128) = reshape(sY, 1, 1, []);
 end
