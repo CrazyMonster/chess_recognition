5f84ff42862b51e11503842bad7db84ed8accb8d
Elia Cereda <eliacereda@gmail.com>
2018-02-21 16:22:31 +0100

    Migliorati i messaggi di errori e corretto il path del modello da caricare in edge_classifier.predict

--- +edge_classifier/predict.m
+++ +edge_classifier/predict.m
@@ -2,19 +2,23 @@ function out = predict(image)
     persistent classifier;
     
     if isempty(classifier)
-        m = load('+edge_classifier/model.compact.mat');
+        m = load('+edge_classifier/model.mat');
         classifier = m.model;
     end
     
     [f, l] = edge_classifier.extract_edge_features(image);
-    c = edge_classifier.compare_edge_regions(f);
     
+    if height(f) == 0
+        error('Nessuna regione trovata.');
+    end
+    
+    c = edge_classifier.compare_edge_regions(f);
     p = classifier.predictFcn(c);
     
     region = edge_classifier.count_comparison_votes(c, p, height(f));
     
     if isnan(region)
-        error('Nessuna regione trovata.');
+        error('Nessuna delle regioni trovate è stata considerata una scacchiera.');
     end
     
     out = (l == region);
