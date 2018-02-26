c389d7cc5742d2f2b6a4592681f8dea68b0f1599
Elia Cereda <eliacereda@gmail.com>
2018-02-26 19:49:41 +0100

    Aggiunta documentazione a recognize_chess_pieces e modificato il valore di default di visualize

--- recognize_chess_pieces.m
+++ recognize_chess_pieces.m
@@ -1,4 +1,15 @@
 function fen = recognize_chess_pieces(image, visualize)
+% RECOGNIZE_CHESS_PIECES Localizza la scacchiera nell'immagine di input
+% IMAGE, riconosce i pezzi e li codifica nella notazione FEN.
+%
+% Se invocata senza valori di ritorno o se VISUALIZE è true, la funzione 
+% visualizza in una figura di MATLAB la posizione della scacchiera 
+% nell'immagine e la configurazione di pezzi riconosciuta.
+%
+% ATTENZIONE: Affinché la visualizzazione funzioni correttamente è 
+% necessario che il font Chess Merida sia installato sul sistema. Una copia
+% del font è fornita con il progetto, nella cartella fonts/.
+
     % Riduci l'immagine a massimo 2048px per lato e convertila in scala 
     % di grigi.
     image = preprocess_image(image, true);
@@ -20,8 +31,9 @@ function fen = recognize_chess_pieces(image, visualize)
     
     % Riconosci i pezzi sulla scacchiera.
     fen = piece_classifier.predict(upright);
-      
-    if nargin < 2 || visualize
+    
+    % Visualizza la scacchiera.
+    if nargout == 0 || (nargin == 2 && visualize)
         plot_board(image, tform, fen);
     end 
 end
